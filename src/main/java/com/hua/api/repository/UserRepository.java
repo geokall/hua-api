1b1184f52b3f2c16091247d565d1f8b2b47089ff
geokall <gkallimanis@iknowhow.com>
2022-08-30 17:11:26 +0300

    Removed unused

--- src/main/java/com/hua/api/repository/UserRepository.java
+++ src/main/java/com/hua/api/repository/UserRepository.java
@@ -2,8 +2,6 @@ package com.hua.api.repository;
 
 import com.hua.api.entity.HuaUser;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -19,8 +17,4 @@ public interface UserRepository extends JpaRepository<HuaUser, Long> {
     Optional<HuaUser> findByMobileNumber(String mobileNumber);
 
     List<HuaUser> findByRoles_Id(Long id);
-
-    @Query( "select u from HuaUser u  where u.roles in :roles" )
-    List<HuaUser> findByRoles(@Param("roles") List<String> roles);
-
 }
